2f6ee600d4879c23d389563c23877cff4b2bc696
Sagar Tamang <mi5t4n@gmail.com>
2024-01-30 13:00:59 +0545

    Populate post fields

--- src/Factories/PostFactory.php
+++ src/Factories/PostFactory.php
@@ -18,14 +18,27 @@ class PostFactory extends Factory{
     }
 
     public function definition() {
+        $post_title = $this->faker->unique()->text();
+
         return array(
-            'post_title' => $this->faker->text(),
-            'post_content' => $this->faker->text(),
-            'post_type' => $this->post_type
+            'post_title' => $post_title,
+            'post_content' => $this->faker->realTextBetween( 100, 360, 5 ),
+            'post_excerpt' => $this->faker->paragraph( $this->faker->numberBetween(1,10)),
+            'post_type' => $this->post_type,
+            'post_status' => $this->faker->randomKey(get_post_stati()),
+            'ping_status' => $this->faker->randomElement(['open', 'closed']),
+            'comment_status' => $this->faker->randomElement(['open', 'closed']),
+            'post_name' => sanitize_title( $post_title),
+            'guid' => site_url(sanitize_title( $post_title )),
+            'post_date' => $this->faker->dateTimeBetween()->format('Y-m-d H:i:s'),
+            'post_date_gmt' => $this->faker->dateTimeBetween()->format('Y-m-d H:i:s'),
+            'post_modified' => $this->faker->dateTimeBetween()->format('Y-m-d H:i:s'),
+            'post_modified_gmt' => $this->faker->dateTimeBetween()->format('Y-m-d H:i:s'),
         );
     }
 
     public function query() {
+
         $count = $this->count;
         $wpdb = $this->wpdb();
 
